5498b37ba611c9609516b280d2d5fe0dd8b66ebd
greg-Da <gregoiredalmasso@gmail.com>
2018-12-19 20:29:28 +0100

    ajout JS

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+setTimeout(function(){ 
+	var XXX = confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please.\nCa te chauffe?");
+	if (XXX == true) {
+		window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+	}
+}, 10000);
