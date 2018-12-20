a81958734900f7f24ede19ef191a9ba2b2ca0bf2
greg-Da <gregoiredalmasso@gmail.com>
2018-12-20 16:15:43 +0100

    AJOUT MODE PSYCHEDELIQUE

--- scripts/main.js
+++ scripts/main.js
@@ -4,3 +4,16 @@ setTimeout(function(){
 		window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
 	}
 }, 10000);
+
+function psyche() {
+	document.querySelectorAll('body').forEach(tag => tag.style.cssText = `
+		background-color: magenta;`)
+
+	 document.querySelectorAll('p').forEach(tag => tag.style.cssText = `
+   		color: blue;
+   		font-family: papyrus, Arial;`)
+
+	 document.querySelectorAll('h1, h2, h3').forEach(tag => tag.style.cssText =`
+	 	color: green;
+	 	font-family: Comic Sans, Arial;`)
+}
